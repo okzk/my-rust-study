7f7fe320a8c508c927e1ee27e2187ef9eaf4c943
okzk <okazaki@okzk.com>
2017-06-16 18:46:39 +0900

    add hello_world_web

---
+++ hello_world_web/src/main.rs
@@ -0,0 +1,11 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hello World!\n")))
+    }
+    Iron::new(hello_world).http("localhost:3000").unwrap();
+}
